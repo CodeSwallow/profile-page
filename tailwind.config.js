5bf9dfad9e3769775c8105097c1ec38ec45fc2e0
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-03-06 11:31:26 -0600

    added and configured tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{html,js}',
+    './src/components/**/*.{html,js}',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
