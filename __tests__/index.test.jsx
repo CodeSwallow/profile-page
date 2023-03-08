7171070dbd40d21c3d6354db27ccc2470ed56ef8
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-03-07 18:42:52 -0600

    updated import paths

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -9,7 +9,7 @@ describe('Home', () => {
         render(<Home />)
 
         const heading = screen.getByRole('heading', {
-            name: /Hello World/i,
+            name: /Home/i,
         })
 
         expect(heading).toBeInTheDocument()
