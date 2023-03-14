16472176b9622cb88075e41e0ae5f3ef37664dfb
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-03-14 17:27:32 -0600

    readded darkmode button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -17,6 +17,13 @@ export default function Navbar({toggleDarkMode}) {
                         <li>
                             <Link href="/">Projects</Link>
                         </li>
+                        <li>
+                            <label className="relative inline-flex items-center cursor-pointer">
+                                <input type="checkbox" value="" className="sr-only peer" onClick={toggleDarkMode}/>
+                                <div
+                                    className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-gray-800 dark:peer-focus:ring-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-600"></div>
+                            </label>
+                        </li>
                     </ul>
                 </div>
             </div>
