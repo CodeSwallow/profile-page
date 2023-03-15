44b431151fdb986db0e497670fff4ba1c8d1b0fc
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-03-15 15:48:44 -0600

    added more tests for Home

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,17 +1,49 @@
 // __tests__/index.test.jsx
 
-import { render, screen } from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import Home from '../src/pages/index'
 import '@testing-library/jest-dom'
 
 describe('Home', () => {
-    it('renders a heading', () => {
+    it('renders correct name in heading', () => {
         render(<Home />)
 
         const heading = screen.getByRole('heading', {
-            name: /Home/i,
+            name: /Isai Ramirez/i,
         })
 
         expect(heading).toBeInTheDocument()
+    })
+
+    it('renders Home link in navbar', () => {
+        render(<Home />);
+
+        const homeLink = screen.getByRole('link', { name: /Home/i});
+
+        expect(homeLink).toBeInTheDocument();
+    })
+
+    it('renders About link in navbar', () => {
+        render(<Home />);
+
+        const aboutLink = screen.getByRole('link', { name: /About/i});
+
+        expect(aboutLink).toBeInTheDocument();
+    })
+
+    it('renders the correct title', () => {
+        render(<Home />);
+
+        const titleElement = screen.getByText('Home', { exact: false });
+
+        expect(titleElement).toBeInTheDocument();
+    })
+
+    it('renders the correct links to Projects', () => {
+        render(<Home />);
+        const linkElement = screen.getAllByRole('link', { name: /projects/i });
+        expect(linkElement[0]).toBeInTheDocument();
+        expect(linkElement[1]).toBeInTheDocument();
+        expect(linkElement).toHaveLength(2);
     })
 })
