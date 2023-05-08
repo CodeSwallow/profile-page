836a749b56ff9b567b0ca2649283d455cee8fc6e
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-05-08 17:41:36 -0600

    feat: updated layout orientation

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import Navbar from "./navbar";
 import { useState, useEffect } from "react";
+import Navbar from "./navbar";
 
 export default function Layout({children}) {
     const [darkMode, setDarkMode] = useState('dark')
@@ -22,7 +22,7 @@ export default function Layout({children}) {
     }
 
     return (
-        <div className={darkMode === "dark" ? "dark" : "light"}>
+        <div className={darkMode === "light" ? "light" : "dark"}>
             <div className="bg-zinc-100 dark:bg-gray-900 min-h-screen min-w-screen">
                 <Head>
                     <meta charSet={"utf-8"}/>
@@ -30,8 +30,16 @@ export default function Layout({children}) {
                     <meta name="viewport" content="width=device-width, initial-scale=1"/>
                     <link rel="icon" href="/favicon.ico"/>
                 </Head>
-                <Navbar toggleDarkMode={toggleDarkMode}/>
-                <main>{children}</main>
+                <div className="flex pt-24 mx-64">
+                    <div className="ml-8 mr-4">
+                        <Navbar toggleDarkMode={toggleDarkMode}/>
+                    </div>
+                    <div className="mr-4">
+                        <main>
+                            {children}
+                        </main>
+                    </div>
+                </div>
             </div>
         </div>
     );
