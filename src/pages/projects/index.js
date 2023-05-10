3ce2e4b03c27d1703afb825fc17429ecffb4131c
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-05-10 11:37:35 -0600

    removed unnecessary import

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import Link from "next/link";
-import Layout from "../../components/layout";
+import Layout from "@/components/layout";
 import ProjectCard from "@/components/projectCard";
 
 export default function Projects() {
