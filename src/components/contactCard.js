e5aae7594d7a33c56b58cfb6631c74499d11bfe7
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-05-16 13:52:34 -0600

    feat: updated contact card

--- src/components/contactCard.js
+++ src/components/contactCard.js
@@ -1,17 +1,24 @@
+import EmailIcon from "@/components/emailIcon";
+import GitHubIcon from "@/components/githubIcon";
+
 export default function ContactCard({contact}) {
+    const contactType = {
+        'Email': <EmailIcon/>,
+        'GitHub': <GitHubIcon/>,
+    }
+
     return (
-        <div className="flex-1 p-4 border bg-zinc-50 rounded-lg shadow dark:bg-gray-800">
-            <div className="text-black dark:text-white flex flex-col justify-center items-center">
-                <div>
-                    {contact.icon}
-                </div>
-                <span className="font-light">
+        <div className="flex-1 p-4 py-8 border bg-zinc-50 rounded-lg shadow dark:bg-gray-800">
+            <a href={contact.url}>
+                <div className="text-black dark:text-white flex flex-col justify-center items-center">
+                    <div>
+                        {contactType[contact.type]}
+                    </div>
+                    <span className="font-light mt-2">
                     {contact.value}
                 </span>
-                <span className="font-light">
-                    {contact.name}
-                </span>
-            </div>
+                </div>
+            </a>
         </div>
     )
 }
