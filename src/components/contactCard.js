41f7e31a53f10ba4e9b539783142c75f96fb19a1
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-05-17 11:55:54 -0600

    feat: added animations

--- src/components/contactCard.js
+++ src/components/contactCard.js
@@ -8,15 +8,15 @@ export default function ContactCard({contact}) {
     }
 
     return (
-        <div className="w-full sm:w-48 mb-8 p-4 py-8 border bg-zinc-50 rounded-lg shadow dark:bg-gray-800">
+        <div className="animate-clear-up flex justify-center w-full sm:w-52 mb-8 p-4 py-8 border bg-zinc-50 rounded-lg shadow dark:bg-gray-800">
             <a href={contact.url}>
-                <div className="text-black dark:text-white flex flex-col justify-center items-center">
+                <div className="animate-pulse text-black dark:text-white flex flex-col justify-center items-center">
                     <div>
                         {contactType[contact.type]}
                     </div>
-                    <span className="font-light mt-2">
-                    {contact.value}
-                </span>
+                    <span className="font-medium mt-2">
+                        {contact.value}
+                    </span>
                 </div>
             </a>
         </div>
--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,15 +1,15 @@
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import TechnologyCard from "@/components/technologyCard";
 
 
-export default function ProjectCard({project}) {
+export default function ProjectCard({project, index}) {
     const [formattedDate, setFormattedDate] = useState('');
 
     useEffect(() => {
         const date = new Date(project.timeline.startDate);
 
-        const month = date.toLocaleString('default', { month: 'long' });
+        const month = date.toLocaleString('default', {month: 'long'});
         const year = date.getFullYear();
 
         const formattedString = `${month} ${year}`;
@@ -17,8 +17,17 @@ export default function ProjectCard({project}) {
         console.log(date)
     }, []);
 
+    const animationClassDelay = {
+        0: 'animate-[right-to-left_0.5s_ease-in-out]',
+        1: 'animate-[right-to-left_0.7s_ease-in-out]',
+        2: 'animate-[right-to-left_0.9s_ease-in-out]',
+        3: 'animate-[right-to-left_1.1s_ease-in-out]',
+        4: 'animate-[right-to-left_1.3s_ease-in-out]'
+    }
+
     return (
-        <div className="flex flex-col justify-between p-4 bg-zinc-50 rounded-lg transition ease-in-out hover:-translate-y-1 hover:scale-105 shadow hover:shadow-lg dark:bg-gray-800">
+        <div
+            className={`${animationClassDelay[index]} flex flex-col justify-between p-4 bg-zinc-50 rounded-lg transition ease-in-out hover:-translate-y-1 hover:scale-105 shadow hover:shadow-lg dark:bg-gray-800`}>
             <div className="text-black dark:text-white flex justify-between mb-2">
                 <span className="text-sm font-light">
                     {formattedDate}
@@ -41,7 +50,7 @@ export default function ProjectCard({project}) {
                 <Link
                     className="text-black dark:text-white hover:text-gray-600 dark:hover:text-gray-200"
                     href={`/projects/${project.sk}`}>
-                        Read more
+                    Read more
                 </Link>
             </div>
         </div>
