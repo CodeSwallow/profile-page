cc3609f5430458edb7752196a41521fc8c0eea40
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-05-18 22:15:32 -0600

    chore: removed unused class

--- src/components/contactCard.js
+++ src/components/contactCard.js
@@ -10,7 +10,7 @@ export default function ContactCard({contact}) {
     return (
         <div className="animate-clear-up transition ease-in-out hover:scale-110 flex justify-center w-full sm:w-52 mb-8 p-4 py-8 border bg-zinc-50 rounded-lg shadow dark:bg-gray-800">
             <a href={contact.url}>
-                <div className="animate-pulse text-black dark:text-white flex flex-col justify-center items-center">
+                <div className="text-black dark:text-white flex flex-col justify-center items-center">
                     <div>
                         {contactType[contact.type]}
                     </div>
